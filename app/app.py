70992804e50fecc5325658fb89f81186e3612224
crawford <crawford.collins@gmail.com>
2019-04-22 17:47:04 -0500

    local website running

--- app/app.py
+++ app/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request
 import pickle
 from fuzzywuzzy import process
 from annoy import AnnoyIndex
-
-
+import boto3
+from decouple import config
 def create_app():
     app = Flask(__name__)
     filename = "songs_df"
@@ -11,15 +11,17 @@ def create_app():
     songs1 = pickle.load(infile)
     infile.close()
     u = AnnoyIndex(300)
-    u.load('app/songs.ann')
-
+    s3_resource = boto3.resource('s3',
+            aws_access_key_id=config("ACCESS_ID"),
+            aws_secret_access_key = config("ACCESS_KEY"))
+    s3_resource.Object('crawftv-music-recommender','songs.ann').download_file('songs1.ann')
+    u.load('songs1.ann')
 
     @app.route('/')
     def root():
-
         return render_template('root.html')
-    @app.route('/search', methods = ["GET"])
 
+    @app.route('/search', methods = ["GET"])
     def search():
         search = request.values["search"]
         search = process.extractOne(search, songs1["song_artist"])[2]
